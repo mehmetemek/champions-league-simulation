0e80564686072f92c1b60783db17c196792bce82
Mehmet Emek <mehmetemekk@gmail.com>
2025-06-13 17:20:59 +0300

    Remove soft delete columns from teams, fixtures, games, and score_boards; update migration to handle reversibility.

---
+++ database/migrations/2025_06_13_135739_remove_soft_deletes_from_tables.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropColumn('deleted_at');
+        });
+
+        Schema::table('fixtures', function (Blueprint $table) {
+            $table->dropColumn('deleted_at');
+        });
+
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropColumn('deleted_at');
+            $table->dropColumn('played_at');
+        });
+
+        Schema::table('score_boards', function (Blueprint $table) {
+            $table->dropColumn('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('fixtures', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+
+        Schema::table('games', function (Blueprint $table) {
+            $table->softDeletes();
+            $table->timestamp('played_at')->after('away_score');
+        });
+
+        Schema::table('score_boards', function (Blueprint $table) {
+            $table->softDeletes();
+        });
+    }
+};
